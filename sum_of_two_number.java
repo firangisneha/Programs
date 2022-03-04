ae1df0ead8d1d06ffe7ac35b890fa78a7666072a
firangisneha <snehafirangi1768@gmail.com>
2022-03-04 16:35:59 +0530

    code for sum_of_two_number

---
+++ sum_of_two_number.java
@@ -0,0 +1,18 @@
+#Program - In-taking two numbers using Scanner and printing their sum
+--> import.java.util.Scanner;
+    class Demo{
+    public static void main(String[]args)
+    {
+    Scanner scan=new Scanner(System.in);
+    System.out.println("enter the first number");
+    int firstnumber=Scan.nextInt();
+    System.out.println("enter the second number");
+    int secondnumber=Scan.nextInt();
+    System.out.println("sum of "+firstnumber+"and"+secondnumber+"is:"+(firstnumber+secondnumber));
+    }
+    }
+    
+OUTPUT:
+enter the first number:5
+enter the second number:7
+The sum of 5 and 7 is:12.
